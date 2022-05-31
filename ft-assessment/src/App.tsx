6fa803efe3741466e9bc35c72531ceccb9eed221
Nelson David Daza <yankeenelson@gmail.com>
2022-05-31 17:17:39 -0400

    get Request with useEffect and useState prototyped, and functional

--- ft-assessment/src/App.tsx
+++ ft-assessment/src/App.tsx
@@ -1,20 +1,25 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import axios from "axios"
 
-interface stateList {
-  stateList: Array<object>
-}
+const covidURL = "https://disease.sh/v3/covid-19/states"
 
-export default class App extends React.Component <{}, stateList> {
-  constructor(props: any){
-    super(props);
-    this.state = {stateList: []}
-  }
 
-  render(){
-    return (
-      <div>Hi</div>
-    )
-  }
+export default function App() {
+  const [states, stateList] = useState(0)
+
+  React.useEffect(() => {
+    axios.get(covidURL).then((response) => {
+      stateList(response.data);
+    });
+  }, []);
+
+  if (!states) return null;
+
+  console.log(states)
+
+  return (
+    <div>hi</div>
+  );
 
   }
