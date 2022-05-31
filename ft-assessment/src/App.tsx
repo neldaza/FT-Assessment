79db097979011e1a1d4f5252e9a9bdeddbe8e2d8
Nelson David Daza <yankeenelson@gmail.com>
2022-05-31 16:52:01 -0400

    Syntax Fixed for TSLint

--- ft-assessment/src/App.tsx
+++ ft-assessment/src/App.tsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import './App.css';
 
-interface StateList {
-
+interface stateList {
+  stateList: Array<object>
 }
 
-export default class App extends React.Component <{}, {stateList: Array<object>}> {
+export default class App extends React.Component <{}, stateList> {
   constructor(props: any){
     super(props);
-    this.state = {stateList: []}}
+    this.state = {stateList: []}
+  }
+
+  render(){
+    return (
+      <div>Hi</div>
+    )
+  }
+
   }
