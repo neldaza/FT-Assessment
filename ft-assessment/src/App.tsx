d774f6c844143e5285ba58fe46fd860969a471c8
Nelson David Daza <yankeenelson@gmail.com>
2022-06-01 00:09:52 -0400

    App completely functional, debating aesthetic improvement

--- ft-assessment/src/App.tsx
+++ ft-assessment/src/App.tsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
 import axios from "axios"
+import { collapseTextChangeRangesAcrossMultipleVersions } from 'typescript';
 
 const covidURL = "https://disease.sh/v3/covid-19/states"
 
@@ -30,33 +31,41 @@ export default function App() {
 
   if (!states) { return null }
 
+
+
   return (
-    <div className='accordion'>
+    <div className='container text-align-center'>
       <h1>COVID-19 Statistics State by State</h1>
-      {states.map((state: any) => {
-        return (
-          <React.Fragment key={state.state}>
-            <div className="row state align-items-center justify-space-around" id={state.state} onClick={handleClick}>
-              <h3>{state.state}</h3>
-              <h4>Cases: {state.cases} </h4>
-            </div>
-            <div className={`state ${currentStateId === state.state ? '' : 'hidden'}`}>
-              <div className='specific-stats'>
-                <p>Cases Today: {state.todayCases}</p>
-                <p>Deaths Today: {state.todayDeaths}</p>
-                <p>Total Deaths: {state.deaths}</p>
-                <p>Active Cases: {state.active}</p>
-                <p>Cases per One Million: {state.casesPerOneMillion}</p>
-                <p>Deaths per One Million: {state.deathsPerOneMillion}</p>
-                <p>Tests per One Million: {state.testsPerOneMillion}</p>
-                <p>Total Tests Taken: {state.tests}</p>
-                <p>Population: {state.population}</p>
-              </div>
-            </div>
-          </React.Fragment>
-        )
-      })}
+      <div className='accordion'>
+      {
+        states.map((state: any, i: any) => {
+            return(
+              <React.Fragment key={state.state}>
+                <div className='specific'>
+                  <div className="main column state align-items-center justify-space-around" id={state.state} onClick={handleClick}>
+                    <h3>{state.state}</h3>
+                    <h4>Cases: {state.cases} </h4>
+                  </div>
+                  <div className={`state ${currentStateId === state.state ? '' : 'hidden'}`}>
+                    <div className='specific-stats'>
+                      <p>Cases Today: {state.todayCases}</p>
+                      <p>Deaths Today: {state.todayDeaths}</p>
+                      <p>Total Deaths: {state.deaths}</p>
+                      <p>Active Cases: {state.active}</p>
+                      <p>Cases per One Million: {state.casesPerOneMillion}</p>
+                      <p>Deaths per One Million: {state.deathsPerOneMillion}</p>
+                      <p>Tests per One Million: {state.testsPerOneMillion}</p>
+                      <p>Total Tests Taken: {state.tests}</p>
+                      <p>Population: {state.population}</p>
+                    </div>
+                  </div>
+                </div>
+              </React.Fragment>
+            )
+         })
+      }
+      </div>
     </div>
   );
 
-  }
+}
